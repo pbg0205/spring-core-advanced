d3adf2e2d5254ab921fdc06ea9854b6f0fe6ad98
cooper <pbg0205@naver.com>
2022-08-12 19:05:08 +0900

    test: Abstract Template Pattern practice

---
+++ log-tracer/src/test/java/com/example/logtracer/trace/template/code/AbstractTemplate.java
@@ -0,0 +1,20 @@
+package com.example.logtracer.trace.template.code;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public abstract class AbstractTemplate {
+
+    // 1. 부가 기능은 추상 클래스로 묶고 핵심 로직은 구현체로 분리시키는 패턴
+    // 2. 변하지 않는 부분은 추상 클래스로 묶고, 변하는 부분은 구현체로 분리시키는 패턴
+    public void execute() {
+        long startTime = System.currentTimeMillis();
+        call();
+        long endTime = System.currentTimeMillis();
+
+        long elapsedTime = endTime - startTime;
+        log.info("elapsedTime: {}", elapsedTime);
+    }
+
+    protected abstract void call();
+}
